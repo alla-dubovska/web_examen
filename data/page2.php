05413b3df255ff6a211e172b6d75cff1a7ba1b9d
dubovska.alla <alusya404@gmail.com>
2016-11-20 21:20:35 +0200

    Add selector for read or not read books.

--- data/page2.php
+++ data/page2.php
@@ -3,9 +3,51 @@ if(!$_SESSION['role']) {
     echo '<h1>Mes livres</h1>';
     echo '<p>Please login first</p>';
 } else {
-    
+?> 
+<form method="post" action="?route=page2">
+    <select name="type_form">
+<?php
+    $selected = $_POST['type_form'];
+    if(!$selected) {
+        $selected = 0;
+    }
+    for ($i = 0; $i<3; $i++) {
+        if($i == $selected) {
+            echo '<option selected ';
+        }
+        else {
+            echo '<option ';
+        }
+        switch ($i) {
+            case 0:
+                echo 'value="0">Tous mes livres</option>';
+                break;
+            
+            case 1:
+                echo 'value="1">Lire des livres</option>';
+                break;
+            
+            case 2:
+                echo 'value="2">Livres non lus</option>';
+                break;
+        }
+    }
+?>
+    </select>
+    <input type="submit" value="Ok" />
+</form>
+<?php       
 $book = array();
-$res = mysqli_query($link, "SELECT * FROM book WHERE user_id = \"" . $_SESSION['id'] . "\"");
+
+$q = "SELECT * FROM book WHERE user_id = \"" . $_SESSION['id'] . "\"";
+
+if($selected == 1) {
+    $q = $q . ' AND is_read = 1';
+} else if ($selected == 2) {
+    $q = $q . ' AND is_read = 0';
+}
+
+$res = mysqli_query($link, $q);
 echo '<table><caption><h1>Mes livres</h1></caption>
                     <tr><th></th>
                     <th>Le nom</th> 
@@ -14,9 +56,9 @@ echo '<table><caption><h1>Mes livres</h1></caption>
                     <th>Le commentaire</th> </tr>';
 while ($row = mysqli_fetch_assoc($res)) {
     if($row[is_read]) {
-        echo "<tr><td><img class=\"done\" src=\"img/ic_done_black_24dp_2x.png\" alt=\"done\"></td>";
+        echo "<tr><td class=\"done\"><img class=\"done\" src=\"img/ic_add_black_24dp_2x.png\" alt=\"done\"></td>";
     } else {
-        echo "<tr><td></td>";
+        echo "<tr><td class=\"done\"><img class=\"done\" src=\"img/ic_remove_black_24dp_2x.png\" alt=\"done\"></td>";
     }
     echo "<td>" . $row['name'] . "</td>";
     echo "<td>" . $row['author'] . "</td>";
